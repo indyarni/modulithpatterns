f4c5e09f7670c9aad4d6fe3027c924ab7864509d
Arnold Franke <arnold.franke@dm.de>
2020-07-03 15:44:09 +0200

    initial spring boot setup

---
+++ src/main/java/de/indyarni/modulithpatterns/ModulithpatternsApplication.java
@@ -0,0 +1,13 @@
+package de.indyarni.modulithpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithpatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulithpatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/indyarni/modulithpatterns/ModulithpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.indyarni.modulithpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithpatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
