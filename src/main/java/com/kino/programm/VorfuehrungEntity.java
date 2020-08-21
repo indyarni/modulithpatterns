2974d77edd628beff86fea3119b5be1135930061
Arnold Franke <arnold.franke@dm.de>
2020-08-21 15:30:14 +0200

    fiddle around with entities

---
+++ src/main/java/com/kino/programm/VorfuehrungEntity.java
@@ -0,0 +1,11 @@
+package com.kino.programm;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class VorfuehrungEntity {
+
+    @Id
+    Long id;
+}
--- src/main/java/com/kino/reservierung/ReservierungEntity.java
+++ src/main/java/com/kino/reservierung/ReservierungEntity.java
@@ -1,19 +1,53 @@
 package com.kino.reservierung;
 
+import com.kino.programm.Vorfuehrung;
+import com.kino.programm.VorfuehrungEntity;
 import lombok.Data;
 import lombok.Value;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Data
+
 @Entity
 class ReservierungEntity {
 
     @Id
     private Long id;
-    private Long vorfuehrungId;
-    private Long sitzreiheId;
+    @ManyToOne
+    private VorfuehrungEntity vorfuehrung;
+    private Long sitzreihe;
     private Integer nummer;
     private boolean abgeholt;
+
+    public VorfuehrungEntity getVorfuehrung() {
+        return vorfuehrung;
+    }
+
+    public void setVorfuehrung(VorfuehrungEntity vorfuehrung) {
+        this.vorfuehrung = vorfuehrung;
+    }
+
+    public Long getSitzreihe() {
+        return sitzreihe;
+    }
+
+    public void setSitzreihe(Long sitzreihe) {
+        this.sitzreihe = sitzreihe;
+    }
+
+    public Integer getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(Integer nummer) {
+        this.nummer = nummer;
+    }
+
+    public boolean isAbgeholt() {
+        return abgeholt;
+    }
+
+    public void setAbgeholt(boolean abgeholt) {
+        this.abgeholt = abgeholt;
+    }
 }
